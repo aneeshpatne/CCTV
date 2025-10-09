96199730d348677f1fa164c209667c050314205a
Aneesh <aneeshpatne@gmail.com>
2025-10-09 07:37:58 +0000

    Refactor encode.py: streamline ffmpeg input parameters and update output URL

--- utilities/encode.py
+++ utilities/encode.py
@@ -1,6 +1,6 @@
 import ffmpeg
 
-ESP_URL = "http://192.168.1.116:81/stream" 
+ESP_URL = "http://192.168.1.116:81/stream"
 
 (
     ffmpeg
@@ -8,25 +8,25 @@ ESP_URL = "http://192.168.1.116:81/stream"
         ESP_URL,
         fflags='nobuffer',
         flags='low_delay',
+        probesize='32k', analyzeduration=0,
         use_wallclock_as_timestamps=1,
-        reconnect=1,
-        reconnect_streamed=1,
-        reconnect_delay_max=2
+        reconnect=1, reconnect_streamed=1, reconnect_delay_max=2
     )
-    .filter('fps', fps=20)
+    .filter('fps', fps=20) 
     .filter('scale', 1280, 720, flags='bicubic')
     .output(
-        'tcp://0.0.0.0:9000?listen=1',
+        'udp://127.0.0.1:9000?pkt_size=1316',
         f='mpegts',
         vcodec='libx264',
-        preset='veryfast',
+        preset='ultrafast',          
         tune='zerolatency',
         pix_fmt='yuv420p',
-        g=40,
-        b='1.2M',
-        maxrate='1.5M',
-        bufsize='2M'
+        g=40,                         
+        x264opts='bframes=0:scenecut=0:keyint=40:min-keyint=40',  
+        b='1.2M', maxrate='1.5M', bufsize='2M',
+        muxdelay=0, muxpreload=0     
     )
+    .global_args('-thread_queue_size', '1024') 
     .overwrite_output()
     .run()
 )
