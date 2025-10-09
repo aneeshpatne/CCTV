623122d2bd7a166267c1a51844587cfb748df62f
Aneesh <aneeshpatne@gmail.com>
2025-10-09 07:54:26 +0000

    Refactor encode.py: clean up formatting and add rotate filter; add initial rotate.py file

--- utilities/encode.py
+++ utilities/encode.py
@@ -12,21 +12,21 @@ ESP_URL = "http://192.168.1.116:81/stream"
         use_wallclock_as_timestamps=1,
         reconnect=1, reconnect_streamed=1, reconnect_delay_max=2
     )
-    .filter('fps', fps=20) 
+    .filter('fps', fps=20)
     .filter('scale', 1280, 720, flags='bicubic')
+    .filter('rotate', 'PI')           # 180° rotation (PI radians)
     .output(
         'udp://127.0.0.1:9000?pkt_size=1316',
         f='mpegts',
         vcodec='libx264',
-        preset='ultrafast',          
+        preset='ultrafast',
         tune='zerolatency',
         pix_fmt='yuv420p',
-        g=40,                         
-        x264opts='bframes=0:scenecut=0:keyint=40:min-keyint=40',  
+        g=40,
+        x264opts='bframes=0:scenecut=0:keyint=40:min-keyint=40',
         b='1.2M', maxrate='1.5M', bufsize='2M',
-        muxdelay=0, muxpreload=0     
+        muxdelay=0, muxpreload=0
     )
-    .global_args('-thread_queue_size', '1024') 
     .overwrite_output()
     .run()
 )
