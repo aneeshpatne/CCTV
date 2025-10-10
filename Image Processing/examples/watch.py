1dfd5a39d409c76f0cc890569159e2fac4e0c4d8
Aneesh <aneeshpatne@gmail.com>
2025-10-10 06:57:37 +0000

    Update watch.py to display current timestamp instead of static text overlay

--- Image Processing/examples/watch.py
+++ Image Processing/examples/watch.py
@@ -1,4 +1,6 @@
 import cv2
+from datetime import datetime
+import pytz
 
 URL = "http://192.168.1.116:81/stream"  
 
@@ -7,13 +9,17 @@ cap = cv2.VideoCapture(URL)
 if not cap.isOpened():
     raise RuntimeError("Could Not Open Stream")
 
+
+ist = pytz.timezone('Asia/Kolkata')
 while True:
     ret, frame = cap.read()
     if not ret:
         raise RuntimeError("Stopped Receiving Frames")
     if cv2.waitKey(1) == ord('q'):
         break
-    cv2.putText(frame, "Aneesh Cam", (10 , 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0,(0, 255, 0),  2, cv2.LINE_AA )
+    current_time = current_time = datetime.now(ist)
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S %p")
+    cv2.putText(frame, formatted_time, (10 , 25), cv2.FONT_HERSHEY_SIMPLEX, 1.0,(0, 255, 0),  2, cv2.LINE_AA )
     cv2.imshow("frame", frame)
 
  
