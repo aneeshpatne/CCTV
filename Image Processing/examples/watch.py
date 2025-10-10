5d105dfc264e134809b412e3471004da362d0f6d
Aneesh <aneeshpatne@gmail.com>
2025-10-10 06:08:59 +0000

    Refactor watch.py: simplify error handling and frame processing logic

--- Image Processing/examples/watch.py
+++ Image Processing/examples/watch.py
@@ -5,20 +5,17 @@ URL = "http://192.168.1.116:81/stream"
 cap = cv2.VideoCapture(URL)
 
 if not cap.isOpened():
-    raise RuntimeError("Could not open video stream. Check Wi-Fi, URL, and that the camera is streaming.")
+    raise RuntimeError("Could Not Open Stream")
 
 while True:
-    ok, frame = cap.read()
-    if not ok:
-        print("Frame grab failed — trying again...")
-        cv2.waitKey(30)
-        continue
-    frame = cv2.rotate(frame, cv2.ROTATE_180)
-    cv2.imshow("ESP32-CAM", frame)
-
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    ret, frame = cap.read()
+    if not ret:
+        raise RuntimeError("Stopped Receiving Frames")
+    
+    cv2.imshow("frame", frame)
+    if cv2.waitKey(1) == ord('q'):
         break
 
+ 
 cap.release()
 cv2.destroyAllWindows()
