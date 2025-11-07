e480078b77a7b8f330e66840e92cadd6f25283b9
Aneesh <aneeshpatne@gmail.com>
2025-11-07 08:15:27 +0000

    Refactor logging for path cleaning process and enhance error handling during file deletion

--- motion/motion.py
+++ motion/motion.py
@@ -1,11 +1,19 @@
 from pathlib import Path
 import logging
+import requests
+import datetime
+import pytz
 
 logging.basicConfig(level=logging.INFO)
 
+ist = pytz.timezone('Asia/Kolkata')
 
 logging.info("Program started")
 directory = Path("data/") 
+
+logging.info("Cleaning Paths")
+
+# Deleting Paths
 
 
 if directory.exists() and directory.is_dir():
@@ -18,3 +26,5 @@ if directory.exists() and directory.is_dir():
                 logging.error(f"[DELETE] Failed to delete {file}: {e}")
 else:
     logging.info("[DELETE] Directory Does not exist")
+
+print(datetime.datetime.now())  # ✅ works
