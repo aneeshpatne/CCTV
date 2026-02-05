ad92359e990b2a255a95196a4238e564adb7bd0e
aneeshpatne <82258226+aneeshpatne@users.noreply.github.com>
2026-02-05 13:37:31 +0530

    Enhance AI summary function with detailed instructions and improved input formatting

--- ai/ai.py
+++ ai/ai.py
@@ -9,8 +9,8 @@ client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 def ai_summary(motion_events):
     response = client.responses.create(
         model="gpt-5-mini",
-        instructions="You are a motion event summariser bot, you will give general comments to the statistics passed to you.",
-        input="How do I check if a Python object is an instance of a class?",
+        instructions="You are a motion event summariser bot, you will give general comments to the statistics passed to you. You will have to format your response as a message sent to the user directly.",
+        input=f"{motion_events} is the motion statistics",
     )
 
     print(response.output_text)
