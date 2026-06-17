2531f0f20801370534de23dd43eb449ea82a1d8e
Aneesh Patne <aneeshpatne@Aneeshs-Mac-mini.local>
2026-06-17 14:04:01 +0530

    Use VideoToolbox in stream encoder utility

--- utilities/encode.py
+++ utilities/encode.py
@@ -1,6 +1,27 @@
+import sys
+
 import ffmpeg
 
 ESP_URL = "http://192.168.0.13:81/stream"
+USE_APPLE_VIDEOTOOLBOX = sys.platform == "darwin"
+
+if USE_APPLE_VIDEOTOOLBOX:
+    H264_OUTPUT_ARGS = {
+        "vcodec": "h264_videotoolbox",
+        "pix_fmt": "nv12",
+        "realtime": 1,
+        "allow_sw": 0,
+        "power_efficient": 1,
+        "prio_speed": 1,
+    }
+else:
+    H264_OUTPUT_ARGS = {
+        "vcodec": "libx264",
+        "preset": "ultrafast",
+        "tune": "zerolatency",
+        "pix_fmt": "yuv420p",
+        "x264opts": "bframes=0:scenecut=0:keyint=40:min-keyint=40",
+    }
 
 (
     ffmpeg.input(
@@ -20,17 +41,13 @@ ESP_URL = "http://192.168.0.13:81/stream"
     .output(
         "udp://127.0.0.1:9000?pkt_size=1316",
         f="mpegts",
-        vcodec="libx264",
-        preset="ultrafast",
-        tune="zerolatency",
-        pix_fmt="yuv420p",
         g=40,
-        x264opts="bframes=0:scenecut=0:keyint=40:min-keyint=40",
         b="1.2M",
         maxrate="1.5M",
         bufsize="2M",
         muxdelay=0,
         muxpreload=0,
+        **H264_OUTPUT_ARGS,
     )
     .overwrite_output()
     .run()
